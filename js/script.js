55d91ad1613ae601c40250a5188f4b32e7342707
kingeski1 <CRISTIANOKINGESKIPEREIRA@GMAIL.COM>
2025-07-05 17:37:19 -0300

    Delete js directory

--- js/script.js
+++
@@ -1,7 +0,0 @@
-
-// Efeito interativo simples ao clicar no título
-document.addEventListener("DOMContentLoaded", function () {
-    document.querySelector("h1").addEventListener("click", function () {
-        alert("Bem-vindo ao Futebol ao Vivo!");
-    });
-});
